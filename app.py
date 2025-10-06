6d28024f7c18a4610af32e2ca19b36cb4c51ab54
PriorTask <156877080+PriorTask@users.noreply.github.com>
2025-10-06 19:08:16 +0530

    fourteenth commit

--- app.py
+++ app.py
@@ -922,7 +922,7 @@ def styled_insight(text):
 
 for fig in [fig1, fig2, fig3, fig4, fig5, fig6, fig7, fig8, fig9, fig10, fig11, fig12, fig14]:
     if fig is not None:
-        fig.update_layout(height=300)
+        fig.update_layout(height=350)
 
 
 col1, col2, col3 = st.columns(3)
